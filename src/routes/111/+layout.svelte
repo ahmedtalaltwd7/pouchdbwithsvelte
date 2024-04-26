1a150b7e937304996f1dc258caf59da9e7925062
Ahmed Talal <76573910+ahmedtalaltwd7@users.noreply.github.com>
2024-04-26 06:57:40 +0300

    add

---
+++ src/routes/111/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  //import "../app.css";
+  import "$lib/preline.js";
+  import { afterNavigate } from "$app/navigation";
+  import { navigating } from "$app/stores";
+
+  import { loading } from "$lib/loading";
+  import Loading from "$lib/loading.svelte";
+
+  $: $loading = !!$navigating;
+
+  // const { afterNavigate } = await import("$app/navigation");
+
+  afterNavigate(() => {
+    window.HSStaticMethods.autoInit(); 
+  });
+</script>
+
+<slot />
+<button on:click={() => ($loading = true)}>Set Loading</button>
+<button on:click={() => ($loading = false)}>Stop Loading</button>
+
+<Loading />
--- src/routes/ssr/+page.svelte
+++ src/routes/ssr/+page.svelte
@@ -12,4 +12,4 @@ dbprueba.info().then(function (info) {
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="./">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="./111">kit.svelte.dev</a> to read the documentation</p>
